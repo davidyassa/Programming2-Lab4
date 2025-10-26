8226d16067a689c47edbd61c777047f5ec9f70e1
abdelrahmanElsayed120 <elsayedabdelrahman220@gmail.com>
2025-10-26 15:32:23 +0300

    main frame,loginframe,homepanel are finished and please rename the loginframe to loginpanel

--- src/frontend/MainFrame.java
+++ src/frontend/MainFrame.java
@@ -3,11 +3,27 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package frontend;
+import javax.swing.*;
 
-/**
- *
- * @author HP
- */
-public class MainFrame {
-    
+public class MainFrame extends JFrame {
+
+    public MainFrame() {
+        this.setTitle("Student Management System");
+        this.setSize(800, 600);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setLocationRelativeTo(null);
+
+        // Show the login screen first
+        setContentPane(new LoginFrame(this));
+
+        this.setVisible(true);
+    }
+
+    public void switchPanel(JPanel panel) {
+        setContentPane(panel);
+        revalidate();
+        repaint();
+    }
+
+   
 }
